040a4509634c6159cbd94118ab13cdc5bcb7d907
unknown <500075823@stu.upes.ac.in>
2021-06-12 10:57:55 +0530

    Test 2

--- main.py
+++ main.py
@@ -6,30 +6,15 @@ Created on Sat Jun 12 10:02:00 2021
 """
 
 import RPi.GPIO as GPIO
-from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(03, GPIO.OUT)
-GPIO.setup(05, GPIO.OUT)
-GPIO.setup(07, GPIO.OUT)
+GPIO.setmode( GPIO.BOARD)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
 
-pwm=GPIO.PWM(07, 100)
-pwm.start(0)
+try:
+	while True:
+		GPIO.output(13, 0)
+		GPIO.output(15, 1)
 
-GPIO.output(03, True)
-GPIO.output(05, False)
-
-pwm.ChangeDutyCycle(50)
-
-GPIO.output(07, True)
-sleep(2)
-GPIO.output(07, False)
-GPIO.output(03, False)
-GPIO.output(05, True)
-
-pwm.ChangeDutyCycle(75)
-GPIO.output(07, True)
-sleep(3)
-GPIO.output(07, False)
-pwm.stop()
-GPIO.cleanup()
+except KeyboardInterrupt:
+	GPIO.cleanup()
