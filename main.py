4eefdde53cf54350cc5685a8df20c989cfa39791
unknown <500075823@stu.upes.ac.in>
2021-06-12 17:06:42 +0530

    Added a Camera

--- main.py
+++ main.py
@@ -6,19 +6,26 @@ Created on Sat Jun 12 10:02:00 2021
 """
 
 import RPi.GPIO as GPIO
+from picamera import PiCamera
+from time import sleep
 
-GPIO.setmode( GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 GPIO.setup(5, GPIO.OUT)
 GPIO.setup(29, GPIO.OUT)
 
-try:
-    while True:
-        GPIO.output(13, 0)
-        GPIO.output(15, 1)
-        GPIO.output(5, 0)
-        GPIO.output(29, 1)
+camera.start_preview()
+sleep(5)
+camera.capture('a.jpg')
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+while True:
+    GPIO.output(13, 0)
+    GPIO.output(15, 1)
+    GPIO.output(5, 1)
+    GPIO.output(29, 0)
+    sleep(5)
+    GPIO.output(13, 0)
+    GPIO.output(15, 0)
+    GPIO.output(5, 0)
+    GPIO.output(29, 0)
