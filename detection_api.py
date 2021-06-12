a7ad0338539682ee25781450a38a420c5b2b40a4
unknown <500075823@stu.upes.ac.in>
2021-06-12 19:23:42 +0530

    Added API Boilerplate

---
+++ detection_api.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 12 19:20:46 2021
+
+@author: Neel
+"""
+
+def detect(img):
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    file_name = os.path.abspath(img)
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+    
+    image = vision.Image(content=content)
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    return labels
+
+
+
+detect = detect('a.jpg')
+print(detect)
