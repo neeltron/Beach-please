ebd79b87bc9e294fd91d4854fae84d0a600cec1f
unknown <500075823@stu.upes.ac.in>
2021-06-12 12:34:39 +0530

    Switching GPIO Pins

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ import RPi.GPIO as GPIO
 GPIO.setmode( GPIO.BOARD)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(29, GPIO.OUT)
 
 try:
     while True:
         GPIO.output(13, 0)
         GPIO.output(15, 1)
-        GPIO.output(17, 0)
-        GPIO.output(27, 1)
+        GPIO.output(5, 0)
+        GPIO.output(29, 1)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
