30e517fc99b9b6bc4f5f637efa5db7a79f718d00
unknown <500075823@stu.upes.ac.in>
2021-06-13 13:44:41 +0530

    Added autostart script

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+
+# /etc/init.d/main.py
+### BEGIN INIT INFO
+# Provides:          main.py
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
 # -*- coding: utf-8 -*-
 """
 Created on Sat Jun 12 10:02:00 2021
